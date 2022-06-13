7069fecd338e7f0ea3298a55dac8a50864813f40
marise-san <marisemrtns@gmail.com>
2022-06-13 11:54:24 -0300

    Add event to buttons and results to every round

--- script.js
+++ script.js
@@ -1,78 +1,57 @@
-// choose the computer's play for the round
-const computerPlay = () => {
-    const arr = ["rock", "paper", "scissors"];
-    const generator = Math.floor(Math.random() * arr.length);
-    const play = arr[generator];
-
-    return play;
-}
-
-// function to play one round and add points to the winner
-const playRound = (player, computer) => {
-    if (player === computer) {
-        result = "It's a tie!";
-    } else if (player == "rock") {
-        if (computer == "paper") {
-            result = "You lost! Paper beats rock.";
-            computerScore++;
-        } else {
-            result = "You won! Rock beats scissors.";
-            playerScore++;
-        }
-    } else if (player == "paper") {
-        if (computer == "scissors") {
-            result = "You lost! Scissors beats paper.";
-            computerScore++;
-        } else {
-            result = "You won! Paper beats rock.";
-            playerScore++;
-        }
-    } else if (player == "scissors") {
-        if (computer == "rock") {
-            result = "You lost! Rock beats scissors.";
-            computerScore++;
-        } else {
-            result = "You won! Scissors beats paper.";
-            playerScore++;
-        }
-    }
-
-    return result;
-
-}
-
-// function that shows the scoreboard and the final result of the game
-const board = (player, computer) => {
-    console.log(`SCOREBOARD:\nPLAYER = ${player} - CPU = ${computer}`);
-
-    if (player === computer) {
-        console.log("Well, it's a tie! Let's try again");
-    } else if (player < computer) {
-        console.log("You lost. Good luck next time!");
-    } else {
-        console.log("You won! Yay");
-    }
-
-}
-
-// function that starts the game and does the loop of the rounds
-const game = () => {
-    for (i = 0; i < 5; i++) {
-        const playerSelection = (prompt("Rock, paper or scissors?")).toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(`ROUND [${i + 1}]`);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`CPU chooses ${computerSelection.toUpperCase()}`);
-    }
-
-    board(playerScore, computerScore);
-
-}
-
-// variables that stores the scoreboard
 let computerScore = 0;
 let playerScore = 0;
+const buttons = document.querySelectorAll('.buttons button');
 
-// calls the function that starts the game
-game();
+const disableButton = () => {
+    buttons.forEach(btn => {
+        btn.disabled = true;;
+})
+};
+
+const computerPlay = () => {
+    const arr = ['rock', 'paper', 'scissors'];
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const playRound = (player) => {
+    let computer = computerPlay();
+    let result = '';
+
+    if ((player == 'rock' && computer == 'scissors') ||
+        (player == 'paper' && computer == 'rock') ||
+        (player == 'scissors' && computer == 'paper')) {
+
+        playerScore += 1;
+        result = `<br>You won this round! ${player} beats ${computer}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
+
+        if (playerScore == 5) {
+            result += '<br><br>Victory!!<br>Refresh your page to play again';
+            disableButton();
+        }
+    }
+    else if (player == computer) {
+        result = `<br>It\'s a tie! ${player} and ${computer}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
+    }
+    else {
+        computerScore += 1;
+        result = `<br>Computer wons this round. ${computer} beats ${player}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`
+
+        if (computerScore == 5) {
+            result += '<br><br>..Oof!...The computers wons the game!<br>Refresh your page to play again.';
+            disableButton();
+        }
+    }
+
+    document.getElementById('result').innerHTML = result;
+    
+    return;
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        playRound(btn.value);
+        
+    });
+});
+
 
