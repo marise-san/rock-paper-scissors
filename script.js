3d5d0de1034622b6a29852efb46bc40cbee27cdb
marise-san <marisemrtns@gmail.com>
2022-06-13 22:35:00 -0300

    Change outputs to match the new index.html

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 let computerScore = 0;
 let playerScore = 0;
-const buttons = document.querySelectorAll('.buttons button');
+const buttons = document.querySelectorAll(".buttons input");
+const round = document.querySelector("#round-result");
+const result = document.querySelector("#result");
+const pScore = document.querySelector(".scoreboard #player #player-score");
+const cScore = document.querySelector(".scoreboard #cpu #cpu-score");
 
 const disableButton = () => {
     buttons.forEach(btn => {
-        btn.disabled = true;;
-})
+        btn.disabled = true;
+    });
 };
 
 const computerPlay = () => {
-    const arr = ['rock', 'paper', 'scissors'];
+    const arr = ["rock", "paper", "scissors"];
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
 const playRound = (player) => {
     let computer = computerPlay();
-    let result = '';
 
-    if ((player == 'rock' && computer == 'scissors') ||
-        (player == 'paper' && computer == 'rock') ||
-        (player == 'scissors' && computer == 'paper')) {
+    if ((player == "rock" && computer == "scissors") ||
+        (player == "paper" && computer == "rock") ||
+        (player == "scissors" && computer == "paper")) {
 
         playerScore += 1;
-        result = `<br>You won this round! ${player} beats ${computer}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
+        pScore.textContent = playerScore;
+        round.textContent = "YOU WIN!";
 
         if (playerScore == 5) {
-            result += '<br><br>Victory!!<br>Refresh your page to play again';
+            result.textContent = "YOU WIN THE GAME!!";
             disableButton();
         }
     }
     else if (player == computer) {
-        result = `<br>It\'s a tie! ${player} and ${computer}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`;
+        round.textContent = "TIE!";
     }
     else {
         computerScore += 1;
-        result = `<br>Computer wons this round. ${computer} beats ${player}<br><br>Player score: ${playerScore}<br>Computer score: ${computerScore}`
+        cScore.textContent = computerScore;
+        round.textContent = "YOU LOSE!";
 
         if (computerScore == 5) {
-            result += '<br><br>..Oof!...The computers wons the game!<br>Refresh your page to play again.';
+            result.textContent = "COMPUTER WIN!";
             disableButton();
         }
     }
 
-    document.getElementById('result').innerHTML = result;
-    
-    return;
 }
 
 buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener("click", () => {
         playRound(btn.value);
-        
     });
 });
 
